cbf1e2f26c505513dcec5a49c7f3691430f48cbf
etreille <tonashss@hotmail.fr>
2017-05-03 09:45:13 +0200

    correction de l'étape 2

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -3,8 +3,10 @@ package edu.lyon1;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,10 +17,10 @@ import java.util.*;
 public class RootController {
 
   @RequestMapping("/")
-  public ModelAndView test(HttpServletRequest request, HttpServletResponse response) {
+  public ModelAndView test(@RequestHeader HttpHeaders headers, HttpServletResponse response) {
     ModelAndView mav = new ModelAndView();
 
-    List<String> headers = new ArrayList<String> (Collections.list(request.getHeaderNames()));
+    List<String> headersList= new ArrayList<String> (headers.keySet());
     mav.addObject("headers",headers);
     mav.addObject("titre", "IUT");
     if (response.getStatus() == HttpStatus.OK.value()){
