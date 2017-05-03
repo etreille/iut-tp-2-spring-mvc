823cb33541bb6515a339e974efcd453bd74df7bc
etreille <tonashss@hotmail.fr>
2017-05-03 09:37:55 +0200

    correction de l'étape 1

--- src/main/java/edu/lyon1/RootController.java
+++ src/main/java/edu/lyon1/RootController.java
@@ -1,19 +1,33 @@
 package edu.lyon1;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.*;
 
 @Controller
 @RequestMapping("/")
 public class RootController {
 
   @RequestMapping("/")
-  public ModelAndView test(HttpServletRequest request) {
+  public ModelAndView test(HttpServletRequest request, HttpServletResponse response) {
     ModelAndView mav = new ModelAndView();
+
+    List<String> headers = new ArrayList<String> (Collections.list(request.getHeaderNames()));
+    mav.addObject("headers",headers);
     mav.addObject("titre", "IUT");
-    mav.addObject("corps", "bonjour");
+    if (response.getStatus() == HttpStatus.OK.value()){
+      mav.addObject("corps", "Accept");
+    }else{
+      mav.addObject("corps", "Fail");
+    }
+
+
     mav.setViewName("template");
     return mav;
   }
